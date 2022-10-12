7dc6fe0c6484b40765d69f46448e9621d484909e
akhtarraja513 <akhtarraja513@gmail.com>
2022-10-12 23:09:33 +0530

    Added More Question in Quiz App

---
+++ src/data.js
@@ -0,0 +1,334 @@
+const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "The International Literacy Day is observed on?",
+      answers: [
+        {
+          text: "Sep 8",
+          correct: true,
+        },
+        {
+          text: "Nov 28",
+          correct: false,
+        },
+        {
+          text: "May 2",
+          correct: false,
+        },
+        {
+          text: "Sep 22",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "The language of Lakshadweep. a Union Territory of India, is",
+      answers: [
+        {
+          text: "Tamil",
+          correct: false,
+        },
+        {
+          text: "Hindi",
+          correct: false,
+        },
+        {
+          text: "Malayalam",
+          correct: true,
+        },
+        {
+          text: "Telugu",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "In which group of places the Kumbha Mela is held every twelve years?",
+      answers: [
+        {
+          text: "Ujjain. Purl; Prayag. Haridwar",
+          correct: false,
+        },
+        {
+          text: "Prayag. Haridwar, Ujjain,. Nasik",
+          correct: true,
+        },
+        {
+          text: "Rameshwaram. Purl, Badrinath. Dwarika",
+          correct: false,
+        },
+        {
+          text: "Chittakoot, Ujjain, Prayag,'Haridwar",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Bahubali festival is related to",
+      answers: [
+        {
+          text: "Islam",
+          correct: false,
+        },
+        {
+          text: "Hinduism",
+          correct: false,
+        },
+        {
+          text: "Buddhism",
+          correct: false,
+        },
+        {
+          text: "Jainism",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Which day is observed as the World Standards  Day?",
+      answers: [
+        {
+          text: "June 26",
+          correct: false,
+        },
+        {
+          text: "Oct 14",
+          correct: true,
+        },
+        {
+          text: "Nov 15",
+          correct: false,
+        },
+        {
+          text: "Dec 2",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Which of the following was the theme of the World Red Cross and Red Crescent Day?",
+      answers: [
+        {
+          text: "'Dignity for all - focus on women'",
+          correct: false,
+        },
+        {
+          text: "Dignity for all - focus on Children'",
+          correct: true,
+        },
+        {
+          text: "Focus on health for all",
+          correct: false,
+        },
+        {
+          text: "Nourishment for all-focus on children",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "September 27 is celebrated every year as",
+      answers: [
+        {
+          text: "Teachers' Day",
+          correct: false,
+        },
+        {
+          text: "National Integration Day",
+          correct: false,
+        },
+        {
+          text: "World Tourism Day",
+          correct: true,
+        },
+        {
+          text: "International Literacy Day",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Who is the author of 'Manas Ka-Hans' ?",
+      answers: [
+        {
+          text: "Khushwant Singh",
+          correct: false,
+        },
+        {
+          text: "Prem Chand",
+          correct: false,
+        },
+        {
+          text: "Jayashankar Prasad",
+          correct: false,
+        },
+        {
+          text: "Amrit Lal Nagar",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "The death anniversary of which of the following leaders is observed as Martyrs' Day?",
+      answers: [
+        {
+          text: "Smt. Indira Gandhi",
+          correct: false,
+        },
+        {
+          text: "PI. Jawaharlal Nehru",
+          correct: false,
+        },
+        {
+          text: "Mahatma Gandhi",
+          correct: true,
+        },
+        {
+          text: "Lal Bahadur Shastri",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Which of the following is observed as Sports Day every year?",
+      answers: [
+        {
+          text: "22nd April",
+          correct: false,
+        },
+        {
+          text: "26th  july",
+          correct: false,
+        },
+        {
+          text: "29th August",
+          correct: true,
+        },
+        {
+          text: "2nd October",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Pongal is a popular festival of which state?",
+      answers: [
+        {
+          text: "Karnataka",
+          correct: false,
+        },
+        {
+          text: "Kerala",
+          correct: false,
+        },
+        {
+          text: "Tamil Nadu",
+          correct: true,
+        },
+        {
+          text: "Andhra Pradesh",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "The first month of the Indian national calendar is",
+      answers: [
+        {
+          text: "Magha",
+          correct: false,
+        },
+        {
+          text: "Chaitra",
+          correct: true,
+        },
+        {
+          text: "Ashadha",
+          correct: false,
+        },
+        {
+          text: "Vaishakha",
+          correct: false,
+        },
+      ],
+    },
+  ];
+
+  export default data;
